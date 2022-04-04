fe5e4f31047858791e845e91af21893245ae7d34
Hsian0709 <100269639+Hsian0709@users.noreply.github.com>
2022-04-04 20:05:45 +0800

    Delete qz3 directory

--- qz3/qz3.js
+++
@@ -1,9 +0,0 @@
-var input= document.getElementById("myinput");
-var output =document.getElementById("output");
-addEventListener("keydown",keylog);
-function keylog(e){
-    if (e.keyCode >= 65 && e.keyCode <= 90 ||
-        e.keyCode >= 96 && e.keyCode <= 105) {
-        output.textContent += `${e.key}`;
-        }
-}
