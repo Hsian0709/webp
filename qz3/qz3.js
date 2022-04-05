7e0558447346e5ce493dfb472e301a333a898576
Hsian0709 <100269639+Hsian0709@users.noreply.github.com>
2022-04-05 23:25:49 +0800

    Update qz3.js

--- qz3/qz3.js
+++ qz3/qz3.js
@@ -2,7 +2,5 @@ var input= document.getElementById("myinput");
 var output =document.getElementById("output");
 input.addEventListener("keydown",keylog);
 function keylog(e){
-    if (e.keyCode >= 32 && e.keyCode <= 126) {
-        output.innerHTML += `${e.key}`;
-        }
+    output.innerHTML += `${e.key}`;
 }
